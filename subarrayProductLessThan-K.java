b3132033251e45985aea7099a6f85ae72da25e64
Krishna Nautiyal <getkrishnanautiyal@gmail.com>
2026-07-08 12:42:02 +0530

    subarrayProductLessThan-K

---
+++ subarrayProductLessThan-K.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        int left=0;
+        int right=0;
+        int count=0;
+        int product=1;
+
+        while(right<nums.length){
+            product*=nums[right];
+
+            while(product>=k){
+                product= product/nums[left];
+                left++;
+            }
+            count+=right-left+1;
+            right++;
+        }
+        return count;
+
+    }
+}
